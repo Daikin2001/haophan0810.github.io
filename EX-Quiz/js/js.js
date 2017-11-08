a44561753cc2893212af2e734bf8e60796cbe2e8
Huu Hao Phan <huuhaotk0810@gmail.com>
2017-11-08 16:48:02 +0700

    qweq

--- EX-Quiz/js/js.js
+++ EX-Quiz/js/js.js
@@ -58,26 +58,75 @@ var question6 ={
 
 }
  var arr = [question1,question2,question3,question4,question5,question6];
-
- var q= document.getElementById("questions");
+ var nQ=document.getElementById("numberQuestion");
+ var q= document.getElementById("question");
  var a1= document.getElementById("btnAnswer1");
  var a2= document.getElementById("btnAnswer2");
  var a3= document.getElementById("btnAnswer3");
  var a4= document.getElementById("btnAnswer4");
+ var foot = document.getElementById("footer");
+ var temp=1;
+ var mark=0;
 
- q.innerHTML = "<h2>"+ arr[0].question+"</h2>";
+ nQ.innerHTML= "Câu số "+temp+":"; 
+ q.innerHTML = arr[0].question;
  a1.innerHTML = arr[0].choose1;
  a2.innerHTML = arr[0].choose2;
  a3.innerHTML = arr[0].choose3;
  a4.innerHTML = arr[0].choose4;
+ foot.innerHTML = "<p class='foot'>Câu số "+temp+ " / "+arr.length+"</p>";
 
  var btnElemt = document.getElementsByClassName("btnAnswer");
  
  for(var i=0; i<btnElemt.length; i++){
  	btnElemt[i].addEventListener("click",function(){
-		for (var j=1; j<arr.length; j++){
+ 		var res=this.innerText;
+ 		if(res==arr[temp-1].answer){
+ 			mark+=1;
+ 		}
+ 		console.log(this.innerText);
+		if(temp<arr.length){
 
+		nQ.innerHTML= "Câu số "+(temp+1)+":"; 
+		 q.innerHTML = arr[temp].question;
+		 a1.innerHTML = arr[temp].choose1;
+		 a2.innerHTML = arr[temp].choose2;
+		 a3.innerHTML = arr[temp].choose3;
+		 a4.innerHTML = arr[temp].choose4;
+ 		foot.innerHTML = "<p class='foot'>Câu số "+(temp+1)+ " / "+arr.length+"</p>";
+
+		}else if(mark==6){
+			var win=document.getElementById("wrap");
+			win.innerHTML= "<h1>Bạn đã chiến thắng, xin chúc mừng</h1>";
+		}else {
+			document.getElementById("questions").style.display="none";
+			document.getElementById("answer").style.display="none";
+			document.getElementById("footer").style.display="none";
+			document.getElementById("resAlert1").innerHTML= "<h1>Rất tiếc bạn đã thất bại</h1>";
+			document.getElementById("resAlert2").innerHTML= "<h3>Bạn đã trả lời đúng "+mark +" câu hỏi</h3>";
+			document.getElementById("resAlert3").innerHTML="<button id='back' onclick='clickBtnBack()'>Chơi lại nào</button>";
 		}
+		
+		temp+=1;
+		 console.log(temp);
 	})
  }
-
+ 
+ function clickBtnBack (){
+ 	document.getElementById("resAlert1").innerHTML= "";
+	document.getElementById("resAlert2").innerHTML= "";
+	document.getElementById("resAlert3").innerHTML= "";
+  	document.getElementById("questions").style.display="block";
+	document.getElementById("answer").style.display="block";
+	document.getElementById("footer").style.display="block";
+	temp=1;
+	mark=0;
+	console.log(temp);
+	nQ.innerHTML= "Câu số "+temp+":"; 
+	 q.innerHTML = arr[0].question;
+	 a1.innerHTML = arr[0].choose1;
+	 a2.innerHTML = arr[0].choose2;
+	 a3.innerHTML = arr[0].choose3;
+	 a4.innerHTML = arr[0].choose4;
+	 foot.innerHTML = "<p class='foot'>Câu số "+temp+ " / "+arr.length+"</p>";
+ }
