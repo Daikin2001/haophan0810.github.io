2abb75b98e5c1f0283325765ba63a0e2f1b5cfcb
Huu Hao Phan <huuhaotk0810@gmail.com>
2017-11-21 17:47:15 +0700

    form - registed

--- Form/js/js.js
+++ Form/js/js.js
@@ -27,23 +27,42 @@
         
         }
     } 
-    var register = {
-        firstName:'',
-        surname:'',
-        mobileNumber:'',
-        email:'',
-        pass:'',
-        birthday:'',
-        sex:''
-    }
+    
     //page registed.html
     var parameters;
-    function processForm()
-    {
-       parameters = location.search.substring(1).split("&");
-      
+    var dataForm=[];
+    parameters = location.search.substring(1).split("&");
+    
+    for (var i=0;i<parameters.length;i++){
+        var index= parameters[i].indexOf('=');
+        console.log(index);
+        var temp;
+        temp=parameters[i].slice(index+1);
+        // dataForm[i]=temp.replace(/+/g,"*");
+        dataForm[i]=decodeURIComponent(temp.split('+').join(' '));
+        
+        
     }
-    processForm();
-    console.log(parameters);
-    $('.info').text(parameters) ;
-
+    var register = {
+        firstName:dataForm[0],
+        surname:dataForm[1],
+        fullName:dataForm[0]+' '+dataForm[1],
+        mobileNumber:dataForm[2],
+        email:dataForm[3],
+        pass:dataForm[4],
+        birthday:dataForm[5]+' - '+dataForm[6]+' - '+dataForm[7],
+        sex:dataForm[8]
+    }
+    console.log(register);
+    $('.info').append('<h2>Full Name: <span>'+register['fullName']+'</span></h2>') ;
+    // $('.info').append('<h2>Surname: <span>'+register['surname']+'</span></h2>') ;
+    
+    $('.info').append('<h2>Mobile Number: <span>'+register['mobileNumber']+'</span></h2>') ;
+    
+    $('.info').append('<h2>Email: <span>'+register['email']+'</span></h2>') ;
+    
+    $('.info').append('<h2>Birthday: <span>'+register['birthday']+'</span></h2>') ;
+    
+    $('.info').append('<h2>Sex: <span>'+register['sex']+'</span></h2>') ;
+    
+  
