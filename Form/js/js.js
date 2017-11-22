e3714863b121572a451c1616c8f427994674c999
Huu Hao Phan <huuhaotk0810@gmail.com>
2017-11-22 12:09:01 +0700

    up

--- Form/js/js.js
+++ Form/js/js.js
@@ -1,31 +1,18 @@
 
-   
+   function clog (a,b){
+       console.log(a + ' : ' +b);
+       console.log('--------------------');
+   }
     for (var i = 1; i<32;i++){
-        if (i==8){
-        $('#day-birthday').append('<option value="'+i+'" selected="1">'+i+'</option>');
-        
-        }else {
         $('#day-birthday').append('<option value="'+i+'" >'+i+'</option>');
-        
-        }
     }
     for (var i = 1; i<13;i++){
-        if (i==10){
-        $('#month-birthday').append('<option value="'+i+'" selected="1">'+i+'</option>');
-        
-        }else {
         $('#month-birthday').append('<option value="'+i+'" >'+i+'</option>');
         
-        }
     } 
     for (var i = 1980; i<2018;i++){
-        if (i==1990){
-        $('#year-birthday').append('<option value="'+i+'" selected="1">'+i+'</option>');
-        
-        }else {
         $('#year-birthday').append('<option value="'+i+'" >'+i+'</option>');
         
-        }
     } 
     
     //page registed.html
@@ -34,8 +21,7 @@
     parameters = location.search.substring(1).split("&");
     
     for (var i=0;i<parameters.length;i++){
-        var index= parameters[i].indexOf('=');
-        console.log(index);
+        var index= parameters[i].indexOf('=');       
         var temp;
         temp=parameters[i].slice(index+1);
         // dataForm[i]=temp.replace(/+/g,"*");
@@ -50,10 +36,10 @@
         mobileNumber:dataForm[2],
         email:dataForm[3],
         pass:dataForm[4],
-        birthday:dataForm[5]+' - '+dataForm[6]+' - '+dataForm[7],
+        birthday:dataForm[5]+'  '+dataForm[6]+'  '+dataForm[7],
         sex:dataForm[8]
     }
-    console.log(register);
+    
     $('.info').append('<h2>Full Name: <span>'+register['fullName']+'</span></h2>') ;
     // $('.info').append('<h2>Surname: <span>'+register['surname']+'</span></h2>') ;
     
@@ -68,5 +54,51 @@
         $('.info').append('<h2>Sex: <span>'+register['sex']+'</span></h2>') ;
     }
     
+// validate
+$('#form-register').on('submit',function(){
+    var isValid = true;
+    
+    // if ($('#full-name').val().match(/(?=^.{6,51}$)([A-Za-z]{1})([A-Za-z0-9!@#$%_\^\&amp;\*\-\.\?]{5,49})$/)==null){
+    //     clog("check",isValid);
+    //     isValid=false;
+
+    // }
+    // if ($('#email').val().match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)==null){
+    //     clog('check email',isValid);
+    //     isValid=false;
+    // }
+   
+    // if ($('#new-password').val().match(/^(?=.*\d).{4,8}$/)==null){
+    //     clog('password',"wrong");
+    //     isValid=false;
+    // }
+   
+    // if ($('#mobile-number').val().match(/^0[0-9]\w{8,9}$/)==null){
+    //     clog('mobile',"false");
+        
+    // }else {
+    //     console.log('else');
+    // }
+    if($('#day-birthday').val()==''){
+        console.log("dasdas");
+        $('#day-birthday').css('border-color','red');
+    }
+    if($('#month-birthday').val()==''){
+        console.log("dasdas");
+        $('#month-birthday').css('border-color','red');
+    }
+    if($('#year-birthday').val()==''){
+        console.log("dasdas");
+        $('#year-birthday').css('border-color','red');
+    }
+    
+    if(register['sex']==undefined){
+        console.log(register['sex']);
+        $('#sex').css('border-width','1px');
+    }
+    return false;
+
+
+})
     
   
