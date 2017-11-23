1784e763a4b1ae59544455e49085c3958e97a67a
Huu Hao Phan <huuhaotk0810@gmail.com>
2017-11-23 18:09:57 +0700

    dasd

--- Table/js/js.js
+++ Table/js/js.js
@@ -52,37 +52,59 @@ var lee = {
     email:'lee@naruto.com',
     phone:'0153456789'
 }
-function sortTable() {
-    var table, rows, switching, i, x, y, shouldSwitch;
+
+function sortTable(n) {
+    
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("myTable");
     switching = true;
-    /*Make a loop that will continue until
-    no switching has been done:*/
+    // Set the sorting direction to ascending:
+    dir = "asc"; 
+    /* Make a loop that will continue until
+    no switching has been done: */
     while (switching) {
-      //start by saying: no switching is done:
+      // Start by saying: no switching is done:
       switching = false;
       rows = table.getElementsByTagName("TR");
-      /*Loop through all table rows (except the
-      first, which contains table headers):*/
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
       for (i = 1; i < (rows.length - 1); i++) {
-        //start by saying there should be no switching:
+        // Start by saying there should be no switching:
         shouldSwitch = false;
-        /*Get the two elements you want to compare,
-        one from current row and one from the next:*/
-        x = rows[i].getElementsByTagName("TD")[0];
-        y = rows[i + 1].getElementsByTagName("TD")[0];
-        //check if the two rows should switch place:
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
         }
       }
       if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
         switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++; 
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
       }
     }
   }
@@ -91,7 +113,13 @@ function loadTable (){
     var table = "<table id='myTable'><caption><span id='caption'>Naruto</span></caption><tr id='row0' >";
     
     for (var i =0;i<arrTittle.length;i++ ){
-        table += "<th id='"+arrTittle[i]+"'>"+arrTittle[i]+"</th>";
+        if(i%2==0){
+            table += "<th onclick='sortTable(0)' id='"+arrTittle[i]+"'>"+arrTittle[i]+"</th>";
+        }else {
+            table += "<th onclick='sortTable(1)' id='"+arrTittle[i]+"'>"+arrTittle[i]+"</th>";
+        }
+
+        
     }
     table += "</tr>";
     var temp=0;
