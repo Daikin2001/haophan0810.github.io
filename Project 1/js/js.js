1ff51968e1f6fa4f696c162fbcb53e8043a4730a
hhaophan <huuhaophan0810@gmail.com>
2017-11-26 15:18:30 +0700

    ds

---
+++ Project 1/js/js.js
@@ -0,0 +1,51 @@
+function changeValue(n) {
+    var m = parseInt(n.value) ;
+    var re1 = 2 * m / Math.log2(m);
+    var result = Math.ceil(re1);     
+    var re3 = Math.round(re1)
+    var re2 = re1.toFixed(1);
+    if (n.value.length>2 | n.value.match(/^\d+$/)==null ){
+        $('#dialog').html("Error! (Nhập số không quá 3 chữ số)");
+    }else {
+        $('#dialog').html('');
+        console.log(re2);
+        $('#result').html(result);
+       
+    }
+    
+    
+}
+
+
+    // var arrImg = ['1','2','3','4']
+    // var temp = 1;
+ 
+    // setInterval(function(){
+        
+    //     if (temp==arrImg.length){
+    //         temp=0;
+    //     }
+    //     temp+=1;
+    //     console.log ("dada");
+    //     var img = '<img src="image/slide'+temp+'.jpg" alt="">';
+    //     $('.slide').html(img);
+    // },4000);
+//
+
+
+///c2
+    
+ var imgs =$('.slide').children('img');
+ console.log(imgs);
+ var count =0;
+ imgs.eq(count).show();
+ 
+ setInterval(function(){
+    imgs.eq(count).fadeOut(1000);
+    count+=1; 
+    imgs.eq(count).fadeIn(1000);
+   if (count==imgs.length){
+       count=-1;
+       imgs.eq(0).fadeIn(1000);
+   }        
+},2500);
