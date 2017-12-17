8635c730fa158c23886afc71f9398f62ec08f335
hhaophan <huuhaophan0810@gmail.com>
2017-12-17 23:32:36 +0700

    LV3

---
+++ Project 2/js/index.js
@@ -0,0 +1,409 @@
+// data product index
+//Special
+var arrDataSpecial =[
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    }
+]
+    
+
+//BÁN cHẠY
+// data product index
+//Special
+var arrDataSelling =[
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+     {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/selling/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+]
+    
+
+//Phu Kien
+
+// data product index
+
+var arrDataAccessories =[
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+     {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+    {
+        name:"Hoa Đặc biệt",
+        tittle:"Hoa Đặc biệt",
+        src:"images/index/acessories/1.jpg",
+        state:"sale",
+        cost:34567,
+        oldCost:3434,
+    },
+]
+var saleOff = [10,20,30,40,50,60];
+    
+$(document).ready(function(){
+    
+    var htmlProductSpecial = "";
+
+    for (var i=0; i<arrDataSpecial.length;i++){
+        htmlProductSpecial+= '<div class="product" id="product-special-'+i+'">'+
+        '<div class="content-product">'+
+        ' <div class="image-product">'+
+            '<a href="">'+
+                '<img src="'+arrDataSpecial[i].src+'" alt="'+arrDataSpecial[i].tittle+'" title="'+arrDataSpecial[i].tittle+'">'+
+            '</a>'+
+        '</div>'+
+        '<div class="descrition">'+
+            '<div class="name-product">'+
+                '<a href="">'+
+                    '<span>Hoa hồng giáng sinh</span>'+
+                '</a>'+
+            '</div>'+
+        '<div class="cost-product">'+
+            '<span class="cost-product-sale">2000 VND</span>'+
+            '<span class="cost-product-not-sale">3000 VND</span>'+
+        '</div>'+
+        '<div class="shopping-cart">'+
+            '<div class="shopping-cart-div">'+
+                '<a href="" class="shopping-cart-a">'+
+                '<i class="fa fa-shopping-cart product-cart" aria-hidden="true"></i>'+
+                '<span class="shopping-cart-span">Thêm vào giỏ hàng</span>'+
+                '</a>'+
+            '</div>'+
+        '</div>'+
+        '</div>'+
+        '<div class="special-product special-product-off">'+
+            '<div class="sale-product">'+
+                '<span>'+saleOff[1]+'% OFF</span>'+
+            '</div>'+
+        '</div>'+        
+        '<div class="special-product special-product-new">'+
+            '<div class="new-product">'+
+                '<span>NEW</span>'+
+            '</div>'+
+        '</div>'+
+        '<div class="special-product special-product-hot">'+
+            '<div class="hot-product">'+
+                '<span>HOT</span>'+
+            '</div>'+
+            '</div>'+        
+        '<div class="quick-view">'+
+            '<a href="">'+
+                '<i class="fa fa-search-plus" aria-hidden="true"></i>'+
+            '</a>'+
+        '</div>'+
+        '</div>'+
+        '</div>'
+
+    }
+    
+    $('#test-add').append(htmlProductSpecial);
+        
+    })
+
+
+
+
+
+
+
+
+
--- Project 2/js/js.js
+++ Project 2/js/js.js
@@ -1,6 +1,199 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    var objHeight = 0;
+    $.each($('.slide-image'), function () {
+
+        objHeight = $(this).height();
+    });
+    $('#slide-images').height(objHeight);
 
 
 
-    
+    //after load, show popup
+    // $('#start-wrap').addClass('show-start-wrap');
+    // $('#show-sale-start').addClass('show-sale-start');
+    // var showPTitleStart = setInterval(function(){
+    //     $('#title-sale-start').addClass('title-sale-start');
+
+    //     clearInterval(showPTitleStart);
+    // },500);
+    // var showContentStart = setInterval(function(){
+    //  $('p.content-sale-start,h1.content-sale-start').addClass('effect-content-sale-start');
+    //     clearInterval(showContentStart);
+    // },800);
+    // //close when click x
+    // $('i.content-sale-start').click(function(){
+    //     $('#start-wrap').removeClass('show-start-wrap');
+    //     console.log(this);;
+    // })
+    // //au to close
+    // var closeStartSale = setInterval(function(){
+    //     $('#start-wrap').removeClass('show-start-wrap');
+
+
+    //     clearInterval(closeStartSale);
+    // },5000);
+
+    //Auto set height for div slide
+    var slideImg = $('.slide-image');
+    var circleSlide= $('.circle-slide');
+
+    //Auto play slide
+    setInterval(function(){
+        $('.slide-image').removeClass('add-transition');        
+        var nextSlide = $('.active').next();
+        var _this = this;
+        console.log(nextSlide.index());
+        $('.active-circle').removeClass('active-circle');
+        
+        if (nextSlide.index() < slideImg.length) {
+            $('.active').addClass('current-slide-out').one('webkitAnimationEnd', function () {
+                $('.current-slide-out').removeClass('current-slide-out');
+            });
+
+            nextSlide.addClass('next-slide-in').one('webkitAnimationEnd', function () {
+                console.log('why');
+                $('.active').removeClass('active');
+
+                $('.next-slide-in').addClass('active').removeClass('next-slide-in');
+              
+            });
+           console.log(nextSlide.index());
+           var index= nextSlide.index();
+        //    $('.active-circle').removeClass('active-circle');
+           $('.circle-slide:nth('+index+')').addClass('active-circle');
+           console.log($('.circle-slide:nth('+index+')').index());
+        } else {
+            $('.active').addClass('current-slide-out').one('webkitAnimationEnd', function () {
+                $('.current-slide-out').removeClass('current-slide-out');
+            });
+            $('.slide-image:first').addClass('next-slide-in').one('webkitAnimationEnd', function () {
+                console.log('why');
+                $('.active').removeClass('active');
+
+                $('.next-slide-in').addClass('active').removeClass('next-slide-in');
+            });
+          
+            $('.circle-slide:first').addClass('active-circle');
+
+        }
+       
+
+    },4000)
+    //Click arrow Next
+    $('#arrow-next').click(function () {
+        $('.slide-image').removeClass('add-transition');        
+        var nextSlide = $('.active').next();
+        var _this = this;
+        console.log(nextSlide.index());
+        $(this).css('pointer-events', 'none');
+        $('.active-circle').removeClass('active-circle');
+        
+        if (nextSlide.index() < slideImg.length) {
+            $('.active').addClass('current-slide-out').one('webkitAnimationEnd', function () {
+                $('.current-slide-out').removeClass('current-slide-out');
+            });
+
+            nextSlide.addClass('next-slide-in').one('webkitAnimationEnd', function () {
+                console.log('why');
+                $('.active').removeClass('active');
+
+                $('.next-slide-in').addClass('active').removeClass('next-slide-in');
+                setTimeout(function () {
+                    $(_this).css('pointer-events', 'auto');
+                }, 500);
+                
+            });
+           console.log(nextSlide.index());
+           var index= nextSlide.index();
+        //    $('.active-circle').removeClass('active-circle');
+           $('.circle-slide:nth('+index+')').addClass('active-circle');
+           console.log($('.circle-slide:nth('+index+')').index());
+        } else {
+            $('.active').addClass('current-slide-out').one('webkitAnimationEnd', function () {
+                $('.current-slide-out').removeClass('current-slide-out');
+            });
+            $('.slide-image:first').addClass('next-slide-in').one('webkitAnimationEnd', function () {
+                console.log('why');
+                $('.active').removeClass('active');
+
+                $('.next-slide-in').addClass('active').removeClass('next-slide-in');
+            });
+            setTimeout(function () {
+                $(_this).css('pointer-events', 'auto');
+            }, 500);
+            $('.circle-slide:first').addClass('active-circle');
+
+        }
+       
+
+    })
+
+    // handle arrow back
+
+    $('#arrow-back').click(function () {
+        $('.slide-image').removeClass('add-transition');
+        
+        var backSlide = $('.active').prev();
+        var _this = this;
+        console.log(backSlide.index());
+        $(this).css('pointer-events', 'none');
+        $('.active-circle').removeClass('active-circle');        
+        if (backSlide.index() < 0) {
+            $('.active').addClass('current-slide-out-right').one('webkitAnimationEnd', function () {
+                $('.current-slide-out-right').removeClass('current-slide-out-right');
+            });
+            $('.slide-image:last').addClass('next-slide-in-left').one('webkitAnimationEnd', function () {
+                console.log('why');
+                $('.active').removeClass('active');
+
+                $('.next-slide-in-left').addClass('active').removeClass('next-slide-in-left');
+            });
+            setTimeout(function () {
+                $(_this).css('pointer-events', 'auto');
+            }, 500);
+            $('.circle-slide:last').addClass('active-circle');
+        } else {
+            $(this).css('pointer-events', 'none');
+            
+                $('.active').addClass('current-slide-out-right').one('webkitAnimationEnd', function () {
+                    $('.current-slide-out-right').removeClass('current-slide-out-right');
+                });
+
+                backSlide.addClass('next-slide-in-left').one('webkitAnimationEnd', function () {
+                    console.log('why');
+                    $('.active').removeClass('active');
+
+                    $('.next-slide-in-left').addClass('active').removeClass('next-slide-in-left');
+                    setTimeout(function () {
+                        $(_this).css('pointer-events', 'auto');
+                    }, 500);
+
+                });
+                var index= backSlide.index();
+                
+           $('.circle-slide:nth('+index+')').addClass('active-circle');
+           
+
+        }
+
+
+    })
+
+    //Click circle in slide
+    $('.circle-slide').click(function(){
+        $('.slide-image').addClass('add-transition');
+        
+        console.log($(this).index());
+        $('.active').removeClass('active');
+        $('.active-circle').removeClass('active-circle');        
+        $(this).addClass('active-circle');        
+        var index = $(this).index();
+        $('.slide-image:nth('+index+')').addClass('active');
+
+        
+        
+
+    })
+
+
 })
