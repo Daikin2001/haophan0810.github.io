2fc47e8a514e6f8901f903b5fb4c4dee2922ac1f
hhaophan <huuhaophan0810@gmail.com>
2018-01-16 23:47:28 +0700

    js

--- flowersvn_shop/js/js3.js
+++ flowersvn_shop/js/js3.js
@@ -1,25 +1,25 @@
-function getElementProduct (arr){
+function getElementProduct(arr) {
     var elmProduct = "";
     var arrLength = arr.length;
-    var test=0;
-    var test2=0;
-    for (var i = 0; i < arrLength; i++) {        
-        test2=(test%4+1)/10;
-        test+=1;        
-        elmProduct += 
-        '<div class="product" id="' + arr[i].id + '">' +
+    var test = 0;
+    var test2 = 0;
+    for (var i = 0; i < arrLength; i++) {
+        test2 = (test % 4 + 1) / 10;
+        test += 1;
+        elmProduct +=
+            '<div class="product" id="' + arr[i].id + '">' +
             '<div class="content-product">' +
             ' <div class="image-product">' +
             '<img src="' + arr[i].src + '" alt="' + arr[i].alt + '" title="' + arr[i].title + '">' +
-            '<div class="img-product-bg">'+
-            '<div class="img-product-bg-a">'+
-            '<a href="./product-description.html" class="img-product-bg-a">'+
-            '</a>'+
-            '<span class="img-product-quick-view" data-id-product="'+arr[i].id+'">'+
-            '<i class="fa fa-plus img-quick-view" aria-hidden="true"></i>'+
-            '</span>'+
-            '</div>'+
-            '</div>'+
+            '<div class="img-product-bg">' +
+            '<div class="img-product-bg-a">' +
+            '<a href="./product-description.html" class="img-product-bg-a">' +
+            '</a>' +
+            '<span class="img-product-quick-view" data-id-product="' + arr[i].id + '">' +
+            '<i class="fa fa-plus img-quick-view" aria-hidden="true"></i>' +
+            '</span>' +
+            '</div>' +
+            '</div>' +
             '</div>' +
             '<div class="descrition">' +
             '<div class="name-product">' +
@@ -28,8 +28,8 @@ function getElementProduct (arr){
             '</a>' +
             '</div>' +
             '<div class="cost-product">' +
-            '<span class="cost-product-sale ' + arr[i].classPromotion + '" data-old-cost="'+arr[i].cost+'">' + arr[i].cost.toLocaleString() + ' đ</span>' +
-            '<span class="cost-product-not-sale" data-current-cost="'+getCurrentCost(arr[i])+'">' + getCurrentCost(arr[i]).toLocaleString() + ' đ</span>' +
+            '<span class="cost-product-sale ' + arr[i].classPromotion + '" data-old-cost="' + arr[i].cost + '">' + arr[i].cost.toLocaleString() + ' đ</span>' +
+            '<span class="cost-product-not-sale" data-current-cost="' + getCurrentCost(arr[i]) + '">' + getCurrentCost(arr[i]).toLocaleString() + ' đ</span>' +
             '</div>' +
             '<div class="rating-product">' +
             '<i class="fa fa-star rating-product" aria-hidden="true"></i>' +
@@ -39,11 +39,11 @@ function getElementProduct (arr){
             '<i class="fa fa-star rating-product" aria-hidden="true"></i>' +
             '</div>' +
             '<div class="shopping-cart">' +
-            '<div class="shopping-cart-div" data-id-product="'+arr[i].id+'">' +
-            
+            '<div class="shopping-cart-div" data-id-product="' + arr[i].id + '">' +
+
             '<i class="fa fa-shopping-cart product-cart" aria-hidden="true"></i>' +
             '<span class="shopping-cart-span">Thêm vào giỏ hàng</span>' +
-            
+
             '</div>' +
             '</div>' +
             '</div>' +
@@ -52,54 +52,54 @@ function getElementProduct (arr){
             '<span class="promotion">' + arr[i].codePromotion + '</span>' +
             '</div>' +
             '</div>' +
-          
+
             '</div>' +
             '</div>'
 
-        }
-        return elmProduct;
     }
-    function getCurrentCost (dataProduct){
-        var currentCost;
-        if(dataProduct.classPromotion == 'promotion-sale'){
-            currentCost= dataProduct.cost - (dataProduct.cost*dataProduct.sale)/100;
-        }else{
-            currentCost=dataProduct.cost;
-        }
-        return Math.round(currentCost);
+    return elmProduct;
+}
+function getCurrentCost(dataProduct) {
+    var currentCost;
+    if (dataProduct.classPromotion == 'promotion-sale') {
+        currentCost = dataProduct.cost - (dataProduct.cost * dataProduct.sale) / 100;
+    } else {
+        currentCost = dataProduct.cost;
     }
-    var numberCart = 0;
-    var arrProductsInCart=[];
-    function addCart (){
-        $('sub.menu-cart').html(numberCart);
-        $('sub.menu-cart').attr('data-number-product',numberCart);
-        $('span.notification-number').html(numberCart+" sản phẩm");
-    }
-    function showCartBottom (){
-        var positionScroll = $("html").scrollTop();
-        
+    return Math.round(currentCost);
+}
+var numberCart = 0;
+var arrProductsInCart = [];
+function addCart() {
+    $('sub.menu-cart').html(numberCart);
+    $('sub.menu-cart').attr('data-number-product', numberCart);
+    $('span.notification-number').html(numberCart + " sản phẩm");
+}
+function showCartBottom() {
+    var positionScroll = $("html").scrollTop();
 
-        // $('.test').html(a);
-        if (positionScroll > 55) {
-            //fix-nav
-            
-            $('#nav-down-wrap').addClass('testScroll');
-            //fix-back-to-top
-            $('div.back-to-top').addClass('show-back-to-top');
-            // console.log( $('sub.menu-cart').attr('data-number-product'));
-            // console.log(numberCart);
-            if ($('sub.menu-cart').attr('data-number-product') != '0') {
-                $('a.cart-scrolled').addClass('show-cart-bottom');
-            }
-          
-        } else {
-            $('#nav-down-wrap').removeClass('testScroll');
-            $('div.back-to-top').removeClass('show-back-to-top');
-            $('a.cart-scrolled').removeClass('show-cart-bottom');
+
+    // $('.test').html(a);
+    if (positionScroll > 55) {
+        //fix-nav
+
+        $('#nav-down-wrap').addClass('testScroll');
+        //fix-back-to-top
+        $('div.back-to-top').addClass('show-back-to-top');
+        // console.log( $('sub.menu-cart').attr('data-number-product'));
+        // console.log(numberCart);
+        if ($('sub.menu-cart').attr('data-number-product') != '0') {
+            $('a.cart-scrolled').addClass('show-cart-bottom');
         }
+
+    } else {
+        $('#nav-down-wrap').removeClass('testScroll');
+        $('div.back-to-top').removeClass('show-back-to-top');
+        $('a.cart-scrolled').removeClass('show-cart-bottom');
     }
-$(document).ready(function(){
-  
+}
+$(document).ready(function () {
+
     addCart();
     // $(window).scroll(function () {
     //     var viewportWidth = $("body").innerWidth();
@@ -108,17 +108,25 @@ $(document).ready(function(){
     //     var b = $(window).scrollTop();
 
     // })
-    var isStatusShortcutMenu=true;
-    $('div#shortcut-menu').click(function(){
+    var isStatusShortcutMenu = true;
+    // click icon menu bar
+    $('div#shortcut-menu').click(function () {
         console.log('click');
-        if(isStatusShortcutMenu){
-        $('div#nav-down-wrap').css('display','block');
-isStatusShortcutMenu=false  ;          
-        }else{
-        $('div#nav-down-wrap').css('display','none');
-isStatusShortcutMenu=true  ;          
-            
+        if (isStatusShortcutMenu) {
+            $('div#nav-down-wrap').css('display', 'block');
+            isStatusShortcutMenu = false;
+        } else {
+            $('div#nav-down-wrap').css('display', 'none');
+            isStatusShortcutMenu = true;
+
         }
+    })
+    //li-level-2 click
+    $('li.list-item.level-2').click(function(){
+        $('ul.menu-level-2').removeClass('show-menu-level-2');
+        var ulChild=$(this).children()[1];
+        $(ulChild).addClass('show-menu-level-2');
+        
     })
     $(window).on('beforeunload', function () {
         $(window).scrollTop(0);
--- template - Copy/js/js.js
+++ template - Copy/js/js.js
@@ -5,50 +5,199 @@
 // output "reverseString("hello how are you");
 
 function reverseString2(str) {
-  
+
     return str.split('').reverse().join('');
-  }
-function reverseString (str){
+}
+function reverseString(str) {
     str = str.split(' ').reverse();
     console.log(str);
     var lengthStr = str.length;
-    for (var i = 0; i<lengthStr;i++){
-        str[i]=str[i].split('').reverse().join('');
+    for (var i = 0; i < lengthStr; i++) {
+        str[i] = str[i].split('').reverse().join('');
         console.log(str[i]);
     }
-    str=str.join(' ');
+    str = str.join(' ');
     console.log(str);
     return str;
 }
 // console.log(reverseString2("how are you"));
 
-function factorialNumber (num){
-    if(num===0){
-        num=1;
-    }else{
-        var isNum =1;
-        for(var i=1;i<num;i++){
-            isNum*=i+1;
+function factorialNumber(num) {
+    if (num === 0) {
+        num = 1;
+    } else {
+        var isNum = 1;
+        for (var i = 1; i < num; i++) {
+            isNum *= i + 1;
         }
-        num=isNum;
+        num = isNum;
     }
     return num;
 }
 // console.log(factorialNumber(10));
 
 function titleCase(str) {
-    str=str.toLowerCase();
-    str=str.split(' ');
-    for(var i=0;i<str.length;i++){
-     m=str[i].split('');
-     
-      m[0]=m[0].toUpperCase();
-      str[i]=m.join('');
-      
+    str = str.toLowerCase();
+    str = str.split(' ');
+    for (var i = 0; i < str.length; i++) {
+        m = str[i].split('');
+
+        m[0] = m[0].toUpperCase();
+        str[i] = m.join('');
+
     }
-    str=str.join(' ');
+    str = str.join(' ');
     return str;
+}
+
+//   titleCase("I'm a little tea pot");
+
+//   Seek and Destroy
+
+
+function destroyer(arr) {
+    // Remove all the values
+    var arrFirst = arguments[0];
+    // convert array-like object to array
+    args = Array.prototype.slice.call(arguments);
+    for (var j = 1; j < args.length; j++) {
+        arr = arr.filter(function (item) {
+            return item !== args[j];
+        })
+        console.log(arr);
+    }
+
+    console.log(arr);
+    return arr;
+}
+
+//   destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+
+// Where do I belong
+function getIndexToIns(arr, num) {
+    var index = 0;
+    arr.sort(function (a, b) {
+        return a - b;
+    })
+    for (var i = 0; i < arr.length; i++) {
+        if (num > arr[i]) {
+            index = i + 1;
+        }
+    }
+    console.log(index);
+    // Find my place in this sorted array.
+    return index;
+}
+
+//   getIndexToIns([40, 60,30,67], 50);
+
+
+//   Caesars Cipher
+
+function rot13(str) { // LBH QVQ VG!    
+    var arrCharCode = [];
+    for (var i = 0; i < str.length; i++) {
+        arrCharCode.push(str.charCodeAt(i));
+    }
+    arrCharCode = arrCharCode.map(function (item) {
+        if (item < 78 && item > 64) {
+            return item + 13;
+        } else if (item < 91 && item > 77) {
+            return item - 13;
+        } else {
+            return item;
+        }
+    })
+    var str2 = "";
+    for (var i = 0; i < arrCharCode.length; i++) {
+        str2 += String.fromCharCode(arrCharCode[i]);
+
+    }
+    console.log(arrCharCode);
+    console.log(str2);
+    str = str2;
+    return str;
+}
+
+// Change the inputs below to test
+// rot13("SERR PBQR PNZC");
+
+
+
+
+// Intermediate Algorithm Scripting
+
+
+// Sum All Numbers in a Range
+
+
+function sumAll(arr) {
+    arr.sort(function (a, b) {
+        return a - b;
+    })
+    console.log(arr);
+    var result = 0;
+    var range = arr[arr.length - 1] - arr[0] + 1;
+    console.log(range);
+    for (var i = arr[0]; i <= arr[1]; i++) {
+        result += i;
+    }
+    console.log(result);
+    return result;
+}
+
+//   sumAll([1, 5]);
+
+// Diff array
+function diffArray(arr1, arr2) {
+    var newArr = [];
+
+    // Same, same; but different.
+
+    for (var i = 0; i < arr1.length; i++) {
+        for (var j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                delete arr1[i];
+                delete arr2[j];
+            }
+        }
+    }
+    var arr = arr1.concat(arr2);
+    // filter element empty
+    newArr = arr.filter(function (item) {
+        return item;
+    })
+    console.log(newArr);
+
+    return newArr;
+}
+
+
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+// Wherefore art thou
+// Global Object
+// Object.prototype.hasOwnProperty()
+// Object.keys()
+
+function whatIsInAName(collection, source) {
+    // What's in a name?
+    var arr = [];
+    // Only change code below this line
+    
+    var arrKeySource = Object.keys(source);
+    for (var i=0; i<collection.length;i++){
+       for(var j=0;j< arrKeySource.length;j++){
+           if{collection[i].hasOwnProperty()}
+       }
+
+    }
+    console.log(collection);
+    console.log(arrKeySource);
+    
+    // Only change code above this line
+    return arr;
   }
   
-  titleCase("I'm a little tea pot");
-  
+  whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 });
