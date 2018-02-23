17b7964c6d7b570380a7a0388a50d767dd277f7f
unknown <huuhaotk0810@gmail.com>
2018-02-23 17:58:39 +0700

    Update data images products

--- flowersvn_shop/js/data/data-flowers-huetay.js
+++ flowersvn_shop/js/data/data-flowers-huetay.js
@@ -1,8 +1,8 @@
-//Database lily-products
-var LL01= {
-    id:'LL01',
+//Database huetay-products
+var HT01= {
+    id:'HT01',
     name:'Lan Song Sinh',
-    category:'lily',
+    category:'huetay',
     description:'Hai cây lan song sinh đến cùng nhau trong sự sắp xếp nhiệt đới hoàn hảo nhất đầy sự trong trẻo và thư giãn. Gửi cái gì đó đặc biệt của một ai đó mà sẽ đặt chúng vào một trance sâu tinh khiết',
     title:'Giỏ hoa Lan Song Sinh',
     alt:'Giỏ hoa Lan Song Sinh',
@@ -11,14 +11,14 @@ var LL01= {
     //promotion sale new hot 
     classPromotion:'promotion-hot',
     sale:10,    
-    src:'images/flowers/lily/LL01.jpg',
+    src:'images/flowers/huetay/HT01.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:'hot'
 }
-var LL02= {
-    id:'LL02',
+var HT02= {
+    id:'HT02',
     name:'Hoa Lan Kép',
-    category:'lily',
+    category:'huetay',
     description:'Rực rỡ rực rỡ, mảng hoa đầy màu sắc rực rỡ này xuất hiện với sức sống tươi sáng và sự quyến rũ sinh động! Chắc chắn để sáng lên bất kỳ phòng, cầu vồng hoa này sẽ làm cho một đơn giản chỉ lộng lẫy',
     title:'Giỏ hoa Hoa Lan Kép',
     alt:'Giỏ hoa Hoa Lan Kép',
@@ -27,14 +27,14 @@ var LL02= {
     //promotion sale new hot 
     classPromotion:'promotion-new',
     sale:10,        
-    src:'images/flowers/lily/LL02.jpg',
+    src:'images/flowers/huetay/HT02.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:'new'
 }
-var LL03= {
-    id:'LL03',
+var HT03= {
+    id:'HT03',
     name:'Hoa Lan Đơn',
-    category:'lily',
+    category:'huetay',
     description:'Thưởng thức Các Pleasures phong phú cho kỳ lạ đã đến trong phong cách! Một món quà ân ái chào mừng sự may mắn, sự thịnh vượng, và sự tỏa sáng dồi dào của người đặc biệt nhất',
     title:'Giỏ hoa Hoa Lan Đơn',
     alt:'Giỏ hoa Hoa Lan Đơn',
@@ -43,14 +43,14 @@ var LL03= {
     //promotion sale new hot 
     classPromotion:'promotion-sale',
     sale:25,        
-    src:'images/flowers/lily/LL03.jpg',
+    src:'images/flowers/huetay/HT03.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:'off'
 }
-var LL04= {
-    id:'LL04',
+var HT04= {
+    id:'HT04',
     name:'Hoa Lan và Hoa Hồng',
-    category:'lily',
+    category:'huetay',
     description:'Hiển thị sự yêu mến của bạn cho một người đáng kể trong cuộc sống của bạn với bộ sưu tập sang trọng này. Hãy tưởng tượng đi thuyền trên Biển Caribê giữa vùng biển trong xanh. Mờ và sáng như bầu trời mùa đông kết hợp tinh tế của các hoa màu trắng và xanh này sẽ tạo nên một món quà bất ngờ tuyệt vời. Hiển thị cảm xúc của bạn để trở thành màu xanh thật sự với những gì họ sẽ nhớ suốt đời.',
     title:'Giỏ hoa Hoa Lan và Hoa Hồng',
     alt:'Giỏ hoa Hoa Lan và Hoa Hồng',
@@ -59,14 +59,14 @@ var LL04= {
     //promotion sale new hot 
     classPromotion:'promotion-hot',
     sale:10,        
-    src:'images/flowers/lily/LL04.jpg',
+    src:'images/flowers/huetay/HT04.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:'hot'
 }
-var LL05= {
-    id:'LL05',
+var HT05= {
+    id:'HT05',
     name:'Hoa Lan Nhật Bản',
-    category:'lily',
+    category:'huetay',
     description:'Từ lâu đài mê hoặc, đã từng có một người phụ nữ trẻ xinh đẹp đến thị trấn Camelot. Được ban phước với vẻ đẹp tinh tế và ân sủng tinh tế, cô bắt đầu câu chuyện về The Princess Diaries. Cảm giác rằng ai đó đặc biệt trong cuộc sống của bạn với một liên lạc của cánh hoa nhạt từ trời!',
     title:'Giỏ hoa The Princess Diaries',
     alt:'Giỏ hoa The Princess Diaries',
@@ -75,14 +75,14 @@ var LL05= {
     //promotion sale new hot 
     classPromotion:'promotion-hot',
     sale:10,        
-    src:'images/flowers/lily/LL05.jpg',
+    src:'images/flowers/huetay/HT05.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:'hot'
 }
-var LL06= {
-    id:'LL06',
+var HT06= {
+    id:'HT06',
     name:'Hoa Lan Tím',
-    category:'lily',
+    category:'huetay',
     description:'Một cái nhìn thoáng qua của những khoảnh khắc Hot Pink sẽ làm sống động sự kiện quan trọng đó! Hương thơm ngọt ngào gọi vườn Eden trong niềm vui suy đồi hoàn mỹ',
     title:'Giỏ hoa Hoa Lan Tím',
     alt:'Giỏ hoa Hoa Lan Tím',
@@ -91,14 +91,14 @@ var LL06= {
     //promotion sale nclassPw hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL06.jpg',
+    src:'images/flowers/huetay/HT06.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL07= {
-    id:'LL07',
+var HT07= {
+    id:'HT07',
     name:'Hoa Lan Rừng',
-    category:'lily',
+    category:'huetay',
     description:'Sự pha trộn tinh tế của những hoa văn trắng tinh khiết, nguyên sơ này với vẻ đẹp tự nhiên và ân sủng. Thêm một chút lấp lánh cho ngày đặc biệt của bạn với những vẻ đẹp hạnh phúc',
     title:'Giỏ hoa Hoa Lan Rừng',
     alt:'Giỏ hoa Hoa Lan Rừng',
@@ -107,14 +107,14 @@ var LL07= {
     //promotion sale nclassPw hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL07.jpg',
+    src:'images/flowers/huetay/HT07.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL08= {
-    id:'LL08',
+var HT08= {
+    id:'HT08',
     name:'Cầu vồng tím',
-    category:'lily',
+    category:'huetay',
     description:'Tận hưởng sự thanh thản của một hòn đảo với những hoa tuyệt đẹp của hoa tươi rực rỡ! Nổi bật với nét quyến rũ kỳ lạ, những nở hoa rực rỡ tạo nên một phong cảnh nhiệt đới',
     title:'Giỏ hoa Cầu vồng tím',
     alt:'Giỏ hoa Cầu vồng tím',
@@ -124,30 +124,30 @@ var LL08= {
     classPromotion:'',
     sale:10,
     oldCost:'',    
-    src:'images/flowers/lily/LL08.jpg',
+    src:'images/flowers/huetay/HT08.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL09= {
-    id:'LL09',
-    name:'A Windfall of lilys',
-    category:'lily',
+var HT09= {
+    id:'HT09',
+    name:'A WindfaHT of huetays',
+    category:'huetay',
     description:'Khuyến khích tinh thần với những bông hoa thanh lịch và quyến rũ này. Tỏa ra niềm vui của vẻ đẹp tinh khiết!',
-    title:'Giỏ hoa A Windfall of lilys',
-    alt:'Giỏ hoa A Windfall of lilys',
+    title:'Giỏ hoa A WindfaHT of huetays',
+    alt:'Giỏ hoa A WindfaHT of huetays',
     rating:4,
     cost:239000,
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL09.jpg',
+    src:'images/flowers/huetay/HT09.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL10= {
-    id:'LL10',
+var HT10= {
+    id:'HT10',
     name:'Ba bông Lan rừng',
-    category:'lily',
+    category:'huetay',
     description:'Kỷ niệm với sự soi rọi thanh lịch như thể đi đến bóng hoàng gia hoặc được phong tước hiệp sĩ trong suốt cuộc đời hiệp sĩ! Hues của hoàng gia bẩm sinh hét to và đẹp trong mắt của ai đó đặc biệt. Đi bộ xuống sân như là một công chúa thực sự được.',
     title:'Giỏ hoa Dreams Of Joy',
     alt:'Giỏ hoa Dreams Of Joy',
@@ -156,14 +156,14 @@ var LL10= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL02.jpg',
+    src:'images/flowers/huetay/HT02.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL11= {
-    id:'LL11',
+var HT11= {
+    id:'HT11',
     name:'Purity Bouquet',
-    category:'lily',
+    category:'huetay',
     description:'Hoàn hảo tinh khiết, sẵn sàng, và breathtakingly đẹp! Vui vẻ hân hoan, chạm vào vẻ đẹp, và tình cảm tinh tế với bó hoa này được sắp xếp một cách chuyên nghiệp bởi một người bán hoa chuyên nghiệp. Nâng tinh thần của họ - gửi ngày hôm nay!',
     title:'Giỏ hoa Purity Bouquet',
     alt:'Giỏ hoa Purity Bouquet',
@@ -172,14 +172,14 @@ var LL11= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL11.jpg',
+    src:'images/flowers/huetay/HT11.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL12= {
-    id:'LL12',
+var HT12= {
+    id:'HT12',
     name:'White Roses',
-    category:'lily',
+    category:'huetay',
     description:'Cổ điển, tinh khiết, và đơn giản là tuyệt đẹp - nói tất cả những gì bạn phải nói với sự thanh lịch ngoạn mục của hoa hồng màu kem.',
     title:'Giỏ hoa White Roses',
     alt:'Giỏ hoa White Roses',
@@ -188,14 +188,14 @@ var LL12= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL12.jpg',
+    src:'images/flowers/huetay/HT12.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL13= {
-    id:'LL13',
+var HT13= {
+    id:'HT13',
     name:'One Dozen Red Roses',
-    category:'lily',
+    category:'huetay',
     description:'Biểu tượng của tình yêu không mệt mỏi, sang trọng dễ dàng với món quà cổ điển của một tá hoa hồng đỏ quyến rũ. Mỗi cánh hoa quý giá đại diện cho một trong vô số lý do rằng cô ấy là người bạn không thể thoát khỏi tâm trí của bạn khi cô ấy phát triển đẹp hơn mỗi năm. Cho cô ấy thấy rằng bạn muốn kỷ niệm sinh nhật của mình một cách hoàn hảo nhất, gửi hoa ngày hôm nay.',
     title:'Giỏ hoa One Dozen Red Roses',
     alt:'Giỏ hoa One Dozen Red Roses',
@@ -204,14 +204,14 @@ var LL13= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL13.jpg',
+    src:'images/flowers/huetay/HT13.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL14= {
-    id:'LL14',
+var HT14= {
+    id:'HT14',
     name:'Perfectly Pleasant',
-    category:'lily',
+    category:'huetay',
     description:'Tinh tế và ngoạn mục là chủ đề trong ngày! Hoàn toàn Pleasant chụp một khoảnh khắc quý giá của hoa hồng, hoa oải hương, và màu vàng hợp tác để làm cho rằng nhân dịp đặc biệt hoàn thành. Hát và nhảy múa với màu sắc của mùa xuân cho thời gian đó của cuộc sống của họ chỉ là xung quanh góc.',
     title:'Giỏ hoa Perfectly Pleasant',
     alt:'Giỏ hoa Perfectly Pleasant',
@@ -220,14 +220,14 @@ var LL14= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL14.jpg',
+    src:'images/flowers/huetay/HT14.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL15= {
-    id:'LL15',
+var HT15= {
+    id:'HT15',
     name:'Fresh Blossom Potpourri',
-    category:'lily',
+    category:'huetay',
     description:'Một mảng chói của hoa tươi thơm được sẵn sàng trên một chiếc nồi đất terra cotta đáng yêu với một ruy băng sáng. Thiết kế hoàn hảo thơm ngon này sẽ mang lại nụ cười hạnh phúc và hạnh phúc sighs! Nói sinh nhật vui vẻ với phong cách và chân thành, gửi hoa!',
     title:'Giỏ hoa Fresh Blossom Potpourri',
     alt:'Giỏ hoa Fresh Blossom Potpourri',
@@ -236,14 +236,14 @@ var LL15= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL15.jpg',
+    src:'images/flowers/huetay/HT15.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL16= {
-    id:'LL16',
+var HT16= {
+    id:'HT16',
     name:'Exotic Scarlet Nights',
-    category:'lily',
+    category:'huetay',
     description:'Nét đẹp cổ điển và thanh lịch tuyệt vời, mảng hoa hồng đỏ rực rỡ này với ân sủng và quyến rũ! Hạnh phúc, nở hoa phong phú luôn luôn làm cho một mùi ngọt ngào và thực sự đáng nhớ món quà sinh nhật!',
     title:'Giỏ hoa Exotic Scarlet Nights',
     alt:'Giỏ hoa Exotic Scarlet Nights',
@@ -252,14 +252,14 @@ var LL16= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL16.jpg',
+    src:'images/flowers/huetay/HT16.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL17= {
-    id:'LL17',
+var HT17= {
+    id:'HT17',
     name:'Butterfly Basket',
-    category:'lily',
+    category:'huetay',
     description:'Mang lại nụ cười vui vẻ với bộ sưu tập đầy màu sắc và niềm vui này! Gửi hoa ngày hôm nay!',
     title:'Giỏ hoa Butterfly Basket',
     alt:'Giỏ hoa Butterfly Basket',
@@ -268,14 +268,14 @@ var LL17= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL17.jpg',
+    src:'images/flowers/huetay/HT17.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL18= {
-    id:'LL18',
+var HT18= {
+    id:'HT18',
     name:'Fashionista Chic',
-    category:'lily',
+    category:'huetay',
     description:'Nhẫn vào mùa xuân với thời trang Fashionista Chic! Một sự hợp tác tinh tế nhất của pastels trong ánh sáng chiếu sáng của màu vàng, hồng, và màu xanh lá cây nổi bật rằng mùa xuân đã đến. Waltz xuống sàn catwalk như thể thế giới đang theo dõi một vẻ đẹp tuyệt đẹp không bao giờ bỏ qua!',
     title:'Giỏ hoa Fashionista Chic',
     alt:'Giỏ hoa Fashionista Chic',
@@ -284,14 +284,14 @@ var LL18= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL02.jpg',
+    src:'images/flowers/huetay/HT02.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL19= {
-    id:'LL19',
+var HT19= {
+    id:'HT19',
     name:'An Exotic Flare of Sunset',
-    category:'lily',
+    category:'huetay',
     description:'Rạng rỡ hoa hồng và một loạt các loại hoa tươi khác đã nở hoa từ sự sắp xếp này trong sắc thái tinh tế của đào và hồng. Đẹp, thơm và sôi động, bó hoa này tạo nên một món quà sinh nhật tuyệt vời cho người đặc biệt!',
     title:'Giỏ hoa An Exotic Flare of Sunset',
     alt:'Giỏ hoa An Exotic Flare of Sunset',
@@ -300,14 +300,14 @@ var LL19= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL19.jpg',
+    src:'images/flowers/huetay/HT19.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL20= {
+var HT20= {
     id:'S20',
     name:'A Little Bit of Blush',
-    category:'lily',
+    category:'huetay',
     description:'Nâng cao kính champagne của bạn trong dịp kỷ niệm sự kiện hoàn hảo đó! Cuộc sống cuộc sống đầy đủ và không bao giờ nhìn lại! Hãy nhấn mạnh khoảnh khắc trong những hương thơm hấp dẫn nhất trong bộ rạn rực rỡ, hoa đào quý báu, và màu trắng rực rỡ để làm cho dịp thích hợp!',
     title:'Giỏ hoa A Little Bit of Blush',
     alt:'Giỏ hoa A Little Bit of Blush',
@@ -316,14 +316,14 @@ var LL20= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL20.jpg',
+    src:'images/flowers/huetay/HT20.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
-var LL21= {
-    id:'LL21',
+var HT21= {
+    id:'HT21',
     name:'Bear Hug Bear',
-    category:'lily',
+    category:'huetay',
     description:'Cánh tay của anh ta tràn ngập một bó hoa tuyệt đẹp, gấu ngựa nhỏ của chúng ta vẫn còn ôm lấy một cái ôm sinh nhật! Các màu hoa hồng và tím quý giá gửi lời chúc mừng sinh nhật của bạn một cách ngọt ngào, trong khi những hoa hồng cổ điển sẽ thích thú giữa các loại hoa tươi thơm.',
     title:'Giỏ hoa Bear Hug Bear',
     alt:'Giỏ hoa Bear Hug Bear',
@@ -332,7 +332,7 @@ var LL21= {
     //promotion sale new hot 
     classPromotion:'',
     sale:10,        
-    src:'images/flowers/lily/LL21.jpg',
+    src:'images/flowers/huetay/HT21.jpg',
      //codePromotion S:sale N:new H:hot D:default
     codePromotion:''
 }
