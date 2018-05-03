d01fb1a26a35044d6e9ab4a2c5eed7ee9fdc2ef0
Huu Hao Phan <huuhaotk0810@gmail.com>
2018-05-04 03:13:04 +0700

    edited

--- demo-highlightjs-froala/js/insert-button.js
+++ demo-highlightjs-froala/js/insert-button.js
@@ -1,7 +1,20 @@
 
-function modify(_this) {
-  console.log('this :', _this);
-  console.log('this.text() :', $(_this).html());
+let isEdit = false;
+let idCodeInput = 1;
+let currentCodeEdit = 0;
+
+function modify(dataCodeInput, _this) {
+  isEdit = true;
+  currentCodeEdit = dataCodeInput;
+  // console.log('this :', _this);
+  // console.log('this.text() :', $(_this).html());
+  let getCodeInput = $(`#code-input-${dataCodeInput} pre code`).text();
+  console.log('getCodeInput :', getCodeInput);
+  // $('#input-code').html("");
+  
+  $('#input-code').html(getCodeInput);
+  $('#highlight-popup').css('display', 'block');
+  // console.log('isEdit :', isEdit);
 }
 
 $(function () {
@@ -53,14 +66,21 @@ $(function () {
       $('#input-code').val("");
       $("#highlight-popup").css("display", "block");
       console.log(getThis);
-      
+
     }
   });
 
 
   $('textarea#froala-content').froalaEditor({
     // Add the custom buttons in the toolbarButtons list, after the separator.
-    toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'help', 'html', '|', 'undo', 'redo', 'insert']
+    toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline',
+      'strikeThrough', 'subscript', 'superscript', '|',
+      'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|',
+      'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent'
+      , 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile',
+      'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR',
+      'selectAll', 'clearFormatting', '|', 'print', 'help', 'html', '|',
+      'undo', 'redo', 'insert']
 
   })
   //insert language to select
@@ -77,20 +97,20 @@ $(function () {
     if (codeInput != '') {
       codeInput = codeInput.replace(/'&'/g, '&amp;')
         .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/&/g,'&amp;');
-        
+        .replace(/>/g, "&gt;");
+
       console.log('codeInput :', codeInput);
       // move to text-container
 
-      $(".text-container").html(`<pre class='${lang} hljs js-insert-text'><code>${codeInput} </code></pre>`);
-      
+      $(".text-container")
+        .html(`<pre class='${lang} hljs js-insert-text'><code>${codeInput} </code></pre>`);
+
       // highlight code of div container
-      $('pre code').each(function (i, block) {
+      $('pre.js-insert-text code').each(function (i, block) {
         console.log(i);
         hljs.highlightBlock(block);
       });
-      
+
       // set attribute : not edit text
       $('pre.js-insert-text').attr("contenteditable", "false");
 
@@ -99,14 +119,39 @@ $(function () {
       // getTextHighlighted.replace(/"<br>"/g,'\n');
       console.log('getTextHighlighted :', getTextHighlighted);
 
-      getThis.html.insert(`${getTextHighlighted}<p></p>`);
+      if (!isEdit) {
+        //insert new code
+        getThis.html.insert(`<div data-id-code-input='${idCodeInput}'>${getTextHighlighted}</div><p></p>`);
+        // insert event double click
+        $(`div[data-id-code-input='${idCodeInput}']`).attr('ondblclick', `modify(${idCodeInput},this)`);
+        //storage code
+        $('#store-code-input')
+          .append(`<div id='code-input-${idCodeInput}'><pre><code>${codeInput}</code><pre></div>`);
+        idCodeInput += 1;
+
+      } else {
+        // edit code highlighted
+        isEdit = false;
+        // change code edited
+        $(`div[data-id-code-input='${currentCodeEdit}']`).html(getTextHighlighted);
+        // change code storaged
+        $(`#code-input-${currentCodeEdit}`).html(`<pre><code>${codeInput}</code></pre>`);
+        console.log('isEdit :', isEdit);
+      }
+      // getThis.html.insert(`<div data-id-code-input='${idCodeInput}'>${getTextHighlighted}</div><p></p>`);
+
+      // getThis.html.insert(`${getTextHighlighted}<p></p>`);
       // getThis.html.insert("<pre><code>" + getTextHighlighted + "</code></pre>");
       // console.log(codeInput);
       // console.log($('#input-code'));
 
       // add function modify
-      $('.js-insert-text').attr('onclick','modify(this)');
+      // $(`div[data-id-code-input='${idCodeInput}']`).attr('ondblclick',`modify(${idCodeInput},this)`);
 
+      // move codeInput to store
+      // $('#store-code-input')
+      // .append(`<div id='code-input-${idCodeInput}'><pre><code>${codeInput}</code><pre></div>`);
+      // idCodeInput+=1;
       //close popup
       $("#highlight-popup").css("display", "none");
 
@@ -120,8 +165,22 @@ $(function () {
 
   // click close
 
-  $('.cancel,.close-icon').click(function(){
-    $('#highlight-popup').css("display","none");
+  $('.cancel,.close-icon').click(function () {
+    if (isEdit) {
+      let dataStore = $(`#code-input-${currentCodeEdit}`).text();
+      let dataCurrent = $(`#input-code`).val();
+      if (dataStore != dataCurrent) {
+        if (confirm("You have changed some options. Are you sure you want to close the dialog window?")) {
+          $('#highlight-popup').css("display", "none");
+          // $('#input-code').val("");
+          
+
+        } else {
+         return;
+        }
+      }
+    }
+    $('#highlight-popup').css("display", "none");
 
 
   })
