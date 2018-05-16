c2fa8f59f354af07720c6d3ef0dc36a85624b21c
haophan0810 <huuhaotk0810@gmail.com>
2018-05-16 11:41:07 +0700

    foods

---
+++ An gi trua nay/js/js.js
@@ -0,0 +1,12 @@
+const CLICK =  document.querySelector('.click');
+const DISPLAY = document.querySelector('.result');
+let foods  = ['Cơm', 'Phở', 'Bánh cuốn', 'Miến trộn', 'Chè', 'Nhịn ăn', 'Cháo', 'Bánh mỳ', 'Bún đậu', 'Bún chả'];
+let length = foods.length;
+
+
+CLICK.addEventListener('click', () => {
+    let index = Math.floor((Math.random() * length));
+    DISPLAY.innerHTML= foods[index];
+
+
+})
