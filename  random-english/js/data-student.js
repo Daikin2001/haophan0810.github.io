f48aed065434e49a424e574476043077e5e276ef
haophan0810 <huuhaotk0810@gmail.com>
2018-06-11 17:46:36 +0700

    random eng

---
+++  random-english/js/data-student.js
@@ -0,0 +1,295 @@
+const trainee7731 = {
+    id: 7731,
+    name:'Hoàng Đạt',
+    userName:'Hoang Dat',
+    phone:'01644944067',
+    mail:'hoangdat1020@gmail.com',
+    level:'js',
+    dayAdmission:'',
+    note:'haft-time'
+}
+const trainee6090 = {
+    id: 6090,
+    name:'Phương Xuân Quang Ngọc',
+    userName:'Phuong Xuan Quang Ngoc',
+    phone:'0965253980',
+    mail:'pxqngoc812@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+const trainee7758 = {
+    id: 7758,
+    name:'Nguyễn Xuân Anh',
+    userName:'Nguyen Xuan Anh',
+    phone:'0975020647',
+    mail:'xuananh22121994@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+const trainee7635 = {
+    id: 7635,
+    name:'Lê Thanh Tâm',
+    userName:'Le Thanh Tam',
+    phone:'0983740529',
+    mail:'thanhtam.svhpu121161@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+const trainee7626 = {
+    id: 7626,
+    name:'Phạm Duy Nguyên',
+    userName:'Pham Duy Nguyen',
+    phone:'01627956210',
+    mail:'nguyentotet34@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7748 = {
+    id: 7748,
+    name:'Nguyễn Nam',
+    userName:'Nguyen Nam',
+    phone:'0963454592',
+    mail:'nguyennamit.94@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7726 = {
+    id: 7726,
+    name:'Trần Văn Sơn',
+    userName:'Tran Van Son',
+    phone:'0962641742',
+    mail:'nosnart1510@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7843 = {
+    id: 7843,
+    name:'Vũ Công Hoàn',
+    userName:'Vu Cong Hoan',
+    phone:'01653566686',
+    mail:'vuhoan6686@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:'haft-time'
+}
+
+const trainee7664 = {
+    id: 7664,
+    name:'Nguyễn Thanh Bình',
+    userName:'Nguyen Thanh Binh',
+    phone:'0966127603',
+    mail:'thanhbinhjrr95@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee4574 = {
+    id: 4574,
+    name:'Nguyễn Ngọc Hải',
+    userName:'Nguyen Ngoc Hai',
+    phone:'01642924035',
+    mail:'ngochaitmt0701@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7627 = {
+    id: 7627,
+    name:'Lưu Hải Đoàn',
+    userName:'Luu Hai Doan',
+    phone:'0934263609',
+    mail:'haidoan9xx@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7591 = {
+    id: 7591,
+    name:'Chu Lập Thành',
+    userName:'Chu Lap Thanh',
+    phone:'0936012410',
+    mail:'jamebuston@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee3398 = {
+    id: 3398,
+    name:'Lê Thành Đạt',
+    userName:'Le Thanh Dat',
+    phone:'0901793701',
+    mail:'ltdat2001alt@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee3517 = {
+    id: 3517,
+    name:'Phạm Thị Yến',
+    userName:'Pham Thi Yen',
+    phone:'0983878805',
+    mail:'yenpt18787@gmail.com',
+    level:'project',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7755 = {
+    id: 7755,
+    name:'Phùng Tuấn Nghĩa',
+    userName:'Phung Nghia',
+    phone:'0967018988',
+    mail:'phungnghia152@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7844 = {
+    id: 7844,
+    name:'Vũ Minh Hiếu',
+    userName:'Vu Minh Hieu',
+    phone:'01687770789',
+    mail:'minhhieu0403.96@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:'haft-time'
+}
+
+const trainee7375 = {
+    id: 7375,
+    name:'Nguyễn Nam Phương',
+    userName:'Nguyen Nam Phuong',
+    phone:'0936361055',
+    mail:'namphuong2217@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7873 = {
+    id: 7873,
+    name:'Hoàng Việt Dũng',
+    userName:'Hoang Viet Dung',
+    phone:'0965658695',
+    mail:'bondonganh@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:'haft-time'
+}
+
+const trainee7846 = {
+    id: 7846,
+    name:'Chu Thị Thanh Nhàn',
+    userName:'Chu Thi Thanh Nhan',
+    phone:'01663698051',
+    mail:'thanhnhan04031992@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7951 = {
+    id: 7951,
+    name:'Nguyễn Đăng Thiện',
+    userName:'Nguyen Dang Thien',
+    phone:'01645529636',
+    mail:'thienlcvip@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7933 = {
+    id: 7933,
+    name:'Trần Hải Dương',
+    userName:'Tran Hai Duong',
+    phone:'0981290586',
+    mail:'haiduong2905@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7942 = {
+    id: 7942,
+    name:'Nguyễn Quyền Anh',
+    userName:'Nguyen Quyen Anh',
+    phone:'01668214573',
+    mail:'nguyenquyenanh199x@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee4766 = {
+    id: 4766,
+    name:'Nguyễn Duy Anh',
+    userName:'Nguyen Duy Anh',
+    phone:'01277392070',
+    mail:'andyanh9x@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+
+
+const trainee8028 = {
+    id: 8028,
+    name:'Trịnh Minh Thúy',
+    userName:'Trinh Minh Thuy',
+    phone:'0973493997',
+    mail:'minhthuy30197@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+
+const trainee7991 = {
+    id: 7991,
+    name:'Lê Trung Nam',
+    userName:'Lê Trung Nam',
+    phone:'0945648620',
+    mail:'namlt3820@gmail.com',
+    level:'js',
+    dayAdmission:'',
+    note:''
+}
+
+
+const trainee7932 = {
+    id: 7932,
+    name:'Nguyễn Đăng Thiện',
+    userName:'Nguyen Dang Thien',
+    phone:'01645529636',
+    mail:'thienlcvip@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
+
+
+const trainee7951 = {
+    id: 7951,
+    name:'Nguyễn Đăng Thiện',
+    userName:'Nguyen Dang Thien',
+    phone:'01645529636',
+    mail:'thienlcvip@gmail.com',
+    level:'html',
+    dayAdmission:'',
+    note:''
+}
